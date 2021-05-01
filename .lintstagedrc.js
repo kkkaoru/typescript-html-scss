f8d585d228339f9caf2abea9ad819cc611dcebce
kaoru.ohtaka <ockacru+git@gmail.com>
2021-05-01 20:47:38 +0900

    Add htmlhint

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,5 +2,5 @@ module.exports = {
   '*.ts': ['eslint --max-warnings=0 --fix', 'prettier -cw'],
   '*.js': ['prettier -cw'],
   '*.scss': ['prettier -cw'],
-  '*.html': ['prettier -cw'],
+  '*.html': ['htmlhint', 'prettier -cw'],
 };

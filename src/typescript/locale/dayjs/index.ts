75780061e6d4054055632809570f9a13d3dc637b
kaoru.ohtaka <ockacru+git@gmail.com>
2021-05-02 01:15:52 +0900

    Clean by eslint

--- src/typescript/locale/dayjs/index.ts
+++ src/typescript/locale/dayjs/index.ts
@@ -1,17 +1,17 @@
 import type { SupportedLocale } from '@/types/locale';
 
-export async function findSupportedDayjsLocale(locale: SupportedLocale = 'ja'):Promise<ILocale> {
-  switch(locale){
+export async function findSupportedDayjsLocale(locale: SupportedLocale = 'ja'): Promise<ILocale> {
+  switch (locale) {
     case 'en':
-      return (await import ('dayjs/locale/en')).default;
+      return (await import('dayjs/locale/en')).default;
     case 'ja':
     default:
-      return (await import ('dayjs/locale/ja')).default;
+      return (await import('dayjs/locale/ja')).default;
   }
 }
 
-export function findSupportedLocaleCodeByLangCode (navigatorLanguageCode: string): SupportedLocale {
-  if (/^en-?/i.test(navigatorLanguageCode)){
+export function findSupportedLocaleCodeByLangCode(navigatorLanguageCode: string): SupportedLocale {
+  if (/^en-?/i.test(navigatorLanguageCode)) {
     return 'en';
   }
   return 'ja';

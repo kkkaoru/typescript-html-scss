f5a508575673f422997c10f1279f92e627d754c6
kaoru.ohtaka <ockacru+git@gmail.com>
2021-05-02 20:50:53 +0900

    Fix mock date time string

--- src/typescript/locale/dayjs/format.test.ts
+++ src/typescript/locale/dayjs/format.test.ts
@@ -5,15 +5,15 @@ import { generateDateTimeText } from './format';
 
 describe('generateDateTimeText', () => {
   // JST 2021-01-01 23:59:59:999
-  const mockUnixTime = 1609513199999;
+  const mockIsoString = '2021-01-01T14:59:59.999Z';
 
   it('should be ja locale text', () => {
-    MockDate.set(mockUnixTime);
+    MockDate.set(mockIsoString);
     expect(generateDateTimeText('ja', jaLocale)).toBe('2021年1月1日 金曜日 23:59:59:999');
     MockDate.reset();
   });
   it('should be en locale text', () => {
-    MockDate.set(mockUnixTime);
+    MockDate.set(mockIsoString);
     expect(generateDateTimeText('en', enLocale)).toBe('Friday, January 1, 2021 PM 11:59:59:999');
     MockDate.reset();
   });

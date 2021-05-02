2c238bf3b32bdc67c46d4707be027fef0efec5bd
kaoru.ohtaka <ockacru+git@gmail.com>
2021-05-02 10:03:53 +0900

    Remove mock unix time

--- src/mocks/date-time/unix-time.ts
+++
@@ -1,2 +0,0 @@
-// JST 2021-01-01 23:59:59:999
-export const mockUnixTime = 1609513199999;
--- src/typescript/locale/dayjs/format.test.ts
+++ src/typescript/locale/dayjs/format.test.ts
@@ -1,10 +1,12 @@
 import jaLocale from 'dayjs/locale/ja';
 import enLocale from 'dayjs/locale/en';
 import MockDate from 'mockdate';
-import { mockUnixTime } from '@/mocks/date-time';
 import { generateDateTimeText } from './format';
 
 describe('generateDateTimeText', () => {
+  // JST 2021-01-01 23:59:59:999
+  const mockUnixTime = 1609513199999;
+
   it('should be ja locale text', () => {
     MockDate.set(mockUnixTime);
     expect(generateDateTimeText('ja', jaLocale)).toBe('2021年1月1日 金曜日 23:59:59:999');

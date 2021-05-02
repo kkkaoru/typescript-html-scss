e01893aa38124129b82d011c0f45830767ee4b57
kaoru.ohtaka <ockacru+git@gmail.com>
2021-05-02 20:59:53 +0900

    Use utc string in test code

--- src/typescript/locale/dayjs/format.test.ts
+++ src/typescript/locale/dayjs/format.test.ts
@@ -4,17 +4,17 @@ import MockDate from 'mockdate';
 import { generateDateTimeText } from './format';
 
 describe('generateDateTimeText', () => {
-  // JST 2021-01-01 23:59:59:999
-  const mockIsoString = '2021-01-01T14:59:59.999Z';
+  // JST 2021-01-01 23:59:59:000
+  const mockUTCString = new Date(2020, 12, 1, 23, 59, 59).toUTCString();
 
   it('should be ja locale text', () => {
-    MockDate.set(mockIsoString);
-    expect(generateDateTimeText('ja', jaLocale)).toBe('2021年1月1日 金曜日 23:59:59:999');
+    MockDate.set(mockUTCString);
+    expect(generateDateTimeText('ja', jaLocale)).toBe('2021年1月1日 金曜日 23:59:59:000');
     MockDate.reset();
   });
   it('should be en locale text', () => {
-    MockDate.set(mockIsoString);
-    expect(generateDateTimeText('en', enLocale)).toBe('Friday, January 1, 2021 PM 11:59:59:999');
+    MockDate.set(mockUTCString);
+    expect(generateDateTimeText('en', enLocale)).toBe('Friday, January 1, 2021 PM 11:59:59:000');
     MockDate.reset();
   });
 });

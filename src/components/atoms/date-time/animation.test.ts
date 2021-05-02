d4a06defcb806433eb4d57a5f7a8cbf17f25d84e
kaoru.ohtaka <ockacru+git@gmail.com>
2021-05-02 20:44:14 +0900

    Add test about animation

--- src/components/atoms/date-time/animation.test.ts
+++ src/components/atoms/date-time/animation.test.ts
@@ -1,8 +1,9 @@
-import { updateDateTimeTextByAnimation } from './animation';
+import jaLocale from 'dayjs/locale/ja';
+import { updateDateTimeTextByAnimation, animateUpdateDateTimeText } from './animation';
 
 describe('updateDateTimeTextByAnimation', () => {
   it('should be executed in callback', async () => {
-    expect.assertions(3);
+    expect.assertions(4);
     const mockNodeList = Object.assign(document.createElement('span'), {
       className: 'datetime',
     } as Partial<Element>).querySelectorAll('*');
@@ -11,5 +12,19 @@ describe('updateDateTimeTextByAnimation', () => {
     expect(mockCallBack.mock.calls).toHaveLength(1);
     expect(mockCallBack.mock.calls.slice().shift()[0]).toBe(mockNodeList);
     expect(mockCallBack.mock.calls.slice().shift()[1]).toBe('ja');
+    expect(mockCallBack.mock.calls.slice().shift()[2]).toBe(jaLocale);
+  });
+});
+
+describe('animateUpdateDateTimeText', () => {
+  it('should be called with requestAnimationFrame', () => {
+    const mockFunction = jest.fn();
+    const spy = jest.spyOn(window, 'requestAnimationFrame').mockImplementation(mockFunction);
+    const mockNodeList = Object.assign(document.createElement('span'), {
+      className: 'datetime',
+    } as Partial<Element>).querySelectorAll('*');
+    animateUpdateDateTimeText(mockNodeList, 'ja', jaLocale);
+    expect(mockFunction.mock.calls).toHaveLength(1);
+    spy.mockRestore();
   });
 });
